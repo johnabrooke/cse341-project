8048e41a3322ce86d1ea74e2151e43667068cf44
John Brooke <johnabrooke@gmail.com>
2021-09-30 20:58:23 -0600

    Add files for the week 3 team activity

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,54 @@
 //TA03 PLACEHOLDER
 const express = require('express');
+const fs = require('fs');
+
 const router = express.Router();
+let items = [];
+//let updateItems = [];
+
+const getData = (tagFilter, cb) => {
+    fs.readFile('./data/data.json', (err, data) => {
+    if (!err) {
+      items = JSON.parse(data);
+    } 
+    if (tagFilter !== '') {
+      items = items.filter(item => item.tags.indexOf(tagFilter) > -1);
+      console.log(tagFilter);
+      //console.log(items);
+    };
+    cb(items);
+  });
+};
 
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+  getData('', (items) => {
+    res.render('pages/ta03', {
+      title: 'Team Activity 03',
+      path: '/ta03', // For pug, EJS
+      items: items
+    });
   });
 });
+
+router.post('/results', (req, res, next) => {
+  let search = req.body.search;
+  //console.log(search);
+  getData(search, (items) => {
+    res.render('pages/ta03', {
+      title: 'Team Activity 03',
+      path: '/ta03', // For pug, EJS
+      items: items
+    })
+  })
+  
+  // updateItems = items.filter(item => {
+  //   item.tag.indexof(search) > -1;
+  // });
+  // console.log(updateItems);
+  // //res.redirect('ta03');
+});
+
+
+
 
 module.exports = router;
